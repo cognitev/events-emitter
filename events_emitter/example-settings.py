012ffd6954e7f39e042c0ec181200cb4933bd033
kareem <kareememad400@gmail.com>
2020-02-04 18:21:31 +0200

    business model schema

--- events_emitter/example-settings.py
+++ events_emitter/example-settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events_emitter'
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,7 @@ DATABASES = {
         },
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
---
+++ events_emitter/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+RULE_STATES = [
+    ('PRESENT', 'PRESENT'),
+    ('ABSENT', 'ABSENT'),
+]
+
+
+class BusinessRules(models.Model):
+    event_type = models.CharField(max_length=80, null=False)
+
+    state = models.CharField(
+        max_length=10,
+        choices=RULE_STATES,
+        null=False
+    )
+
+    duration = models.DurationField()
+
+
+class EventsDependencies(models.Model):
+    dependency_experssion = models.CharField(max_length=250, null=False)
