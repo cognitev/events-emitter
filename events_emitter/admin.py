32ffc96fbee1b97e1893415a0dae134841c11042
Kareem Emad <kareememad400@gmail.com>
2020-02-06 16:35:20 +0200

    INS 4376 emitter rules schema design (#1)
    * app setup
    
    * business model schema
    
    * dashboard for creating events

---
+++ events_emitter/admin.py
@@ -0,0 +1,15 @@
+from events_emitter.models import BusinessRules, EventsDependencies
+from django.contrib import admin
+from events_emitter.signals import *# noqa
+
+
+class BusinessRulesAdmin(admin.ModelAdmin):
+    fields = ['event_type', 'state', 'duration']
+
+
+class EventsDependenciesAdmin(admin.ModelAdmin):
+    fields = ['dependency_experssion']
+
+
+admin.site.register(BusinessRules, BusinessRulesAdmin)
+admin.site.register(EventsDependencies, EventsDependenciesAdmin)
---
+++ events_emitter/urls.py
@@ -0,0 +1,48 @@
+"""events_emitter URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url, include
+from tastypie.resources import ModelResource
+from events_emitter.models import BusinessRules, EventsDependencies
+from django.utils.translation import ugettext_lazy
+
+
+class BusinessRulesResource(ModelResource):
+    class Meta:
+        queryset = BusinessRules.objects.all()
+        resource_name = 'business_rule'
+
+
+class EventsDependenciesResource(ModelResource):
+    class Meta:
+        queryset = EventsDependencies.objects.all()
+        resource_name = 'events_dependency'
+
+
+business_rule_resource = BusinessRulesResource()
+event_dependency_resource = EventsDependenciesResource()
+
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+    url(r'^api/', include(business_rule_resource.urls)),
+    url(r'^api/', include(event_dependency_resource.urls)),
+]
+
+admin.site.site_header = ugettext_lazy('Events Emitter Dashboard')
+admin.site.index_title = ugettext_lazy('Business Events administration')
+admin.site.site_header = ugettext_lazy('Events Emitter')
+admin.site.site_title = ugettext_lazy('Events Emitter')
